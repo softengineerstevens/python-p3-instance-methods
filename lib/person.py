3189a0db8b9014d075e7824bfe721c01e3ea4fa8
softengineerstevens <softengineerstevens@gmail.com>
2024-09-03 10:04:53 +0300

    instance methods

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
     pass
+    # Class body goes here
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+    #Instance method definition
+steve = Person()
+steve.talk()
+steve.walk()
